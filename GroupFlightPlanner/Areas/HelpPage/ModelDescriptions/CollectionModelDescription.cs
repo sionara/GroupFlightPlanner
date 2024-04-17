cefb67d32e7f5f5b390c7218bedfff8a81a566e1
Arnulfo Sanchez <arnulfo457@gmail.com>
2024-04-17 03:26:25 -0400

    Extra feature - Implement ASP.NET Web API Help Page

---
+++ GroupFlightPlanner/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace GroupFlightPlanner.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ GroupFlightPlanner/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GroupFlightPlanner.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
